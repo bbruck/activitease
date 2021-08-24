2719ed3c8886f2dc829ea74a286fe6e0bc5258e5
Bernard Bruck <bernard.bruck@gmail.com>
2021-08-24 17:26:04 +0200

    Add cloudinary & views for activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,36 @@
 class ActivitiesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+
+  def index
+    @activities = Activity.all
+  end
+
+  def show
+    @activity = Activity.find(params[:id])
+  end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    @activity.user = current_user
+    if @activity.save!
+      redirect_to activity_path(@activity)
+    else
+      render :new
+    end
+  end
+
+  def save
+    @activity.save
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:title, :description, :category, :subcategory, :date_from, :date_to, :adress, :photo)
+  end
+
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,5 @@ class Activity < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews
+  has_one_attached :photo
 end
