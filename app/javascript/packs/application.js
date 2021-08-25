176de439225a804d30dbcad197a1fdaf8bb15d96
Sascha Grimmer <sascha.grimmer@gmail.com>
2021-08-25 12:19:24 +0200

    Geocoding & Map Display

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,9 +26,14 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
+// CSS
+import 'mapbox-gl/dist/mapbox-gl.css';
+// internal imports
+import { initMapbox } from '../plugins/init_mapbox';
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initMapbox();
 });
