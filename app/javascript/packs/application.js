46e6fa2ad945ec9ca17f91fb4d768754806c35eb
Sascha Grimmer <sascha.grimmer@gmail.com>
2021-08-26 16:25:41 +0200

    Application.js is back

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,42 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+
+// ----------------------------------------------------
+// Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
+// WRITE YOUR OWN JS STARTING FROM HERE 👇
+// ----------------------------------------------------
+
+// External imports
+import "bootstrap";
+import { initAutocomplete } from "../plugins/init_autocomplete";
+
+// Internal imports, e.g:
+// CSS
+import 'mapbox-gl/dist/mapbox-gl.css';
+// internal imports
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import { initMapbox } from '../plugins/init_mapbox';
+// import { initSelect2 } from '../components/init_select2';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your functions here, e.g:
+  // initSelect2();
+  initMapbox();
+  initAutocomplete();
+});
