14db399e36a12753a5c57a847865b2cc11e46906
Bernard Bruck <bernard.bruck@gmail.com>
2021-08-27 13:14:45 +0200

    Booking dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,26 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @activities = Activity.where(user: current_user)
   end
 
+  def accept(booking)
+    @booking = Booking.find(booking)
+    @booking.status = "accepted"
+    @booking.save
+    redirect_to dashboard_path
+  end
+
+  def decline(booking)
+    @booking = Booking.find(booking)
+    @booking.status = "declined"
+    @booking.save
+    redirect_to dashboard_path
+  end
+
+
+  def save
+    @booking.save
+  end
 
 end
