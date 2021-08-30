38ea0081aa9ff99ebd9500245e4399d3f9103213
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-08-30 17:44:03 +0200

    Add sanitizer for additional parameter :nickname

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
   def initialize_message
     @message = Message.new
   end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+  end
 end
