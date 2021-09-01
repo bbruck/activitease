508f1fb473a125a3441d1b78d172666117263055
Bernard Bruck <bernard.bruck@gmail.com>
2021-09-01 11:50:16 +0200

    Try to make image mandatory

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,7 +4,8 @@ class Activity < ApplicationRecord
   has_many :users, through: :bookings
   has_many :reviews, dependent: :destroy
   has_one_attached :photo
-  validates :photo, presence: true
+  # validates :title, presence: true
+  # validates :photo, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   CATEGORIES = ['Adventure', 'Animal', 'Architecture', 'Arts', 'Collectibles', 'Culture', 'Education', 'Food', 'Games', 'Green', 'Handicrafts', 'Music', 'Observation', 'Social']
